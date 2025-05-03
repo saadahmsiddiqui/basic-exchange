f9501cabff60ca9bad05ef3a9cdefd4ab0354f2d
Saad Ahmed Siddiqui <saadahm.siddiqui@gmail.com>
2025-05-03 20:53:45 +0200

    chore: minor improvement

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -2,14 +2,21 @@ import OrderBook from "@/components/orderbook";
 import Image from "next/image";
 
 export default async function Home() {
+  let orderbook = null;
 
-  const data = await fetch("http://localhost:3001/api/orderbook");
-  const orderbook = await data.json();
+  try {
+    const data = await fetch("http://localhost:3001/api/orderbook");
+    orderbook = await data.json();    
+  } catch (error) {
+    console.error(error)
+  }
+
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <OrderBook orderbook={orderbook}></OrderBook>
+        {orderbook && <OrderBook orderbook={orderbook}></OrderBook>}
+        {!orderbook && <p>Please enable the engine server!</p>}
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
@@ -25,7 +32,7 @@ export default async function Home() {
             width={16}
             height={16}
           />
-          Find my here
+          Find my details here
         </a>
       </footer>
     </div>
